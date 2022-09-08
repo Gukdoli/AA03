641b254e936356b9c7195a4897ae52728135ce82
김성우 <100755199+Gukdoli@users.noreply.github.com>
2022-09-08 16:26:32 +0900

    Add files via upload

---
+++ aa03_rpt02/start/hello_function.js
@@ -0,0 +1,7 @@
+// hello_function.js
+function hello(what) {
+    console.log("Hello " + what + " !");
+}
+
+hello("aa03");
+hello("Gukdoli, 김성우");
---
+++ aa03_rpt02/start/index_aa03.js
@@ -0,0 +1,5 @@
+var myinfo = require('./aa03info');
+
+myinfo("aa00", "Redwoods", '010-1234-5678');
+
+myinfo("aa03", "김성우", '010-2446-1676');
